ed3b7444807274b94ef5230e4f3ae1388f772d62
Debondt Didier <p@henix.be>
2016-02-16 10:15:34 +0100

    Règle de codage

--- agendasa_fonctions.php
+++ agendasa_fonctions.php
@@ -9,13 +9,6 @@
  * @package    SPIP\Agendasa\Fonctions
  */
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
-
-/*
- * Un fichier de fonctions permet de définir des éléments
- * systématiquement chargés lors du calcul des squelettes.
- *
- * Il peut par exemple définir des filtres, critères, balises, …
- * 
- */
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
