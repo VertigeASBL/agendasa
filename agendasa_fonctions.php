5ff49394460c1de782071aa609e297715ecd8829
Debondt Didier <p@henix.be>
2016-02-16 10:52:42 +0100

    Règle de codage

--- agendasa_fonctions.php
+++ agendasa_fonctions.php
@@ -57,34 +57,43 @@ function formulaires_editer_evenement_verifier(
 	return $erreurs;
 }
 
-function formulaires_editer_evenement_traiter($id_evenement='new', $id_article=0, $retour='', $lier_trad = 0, $config_fonc='evenements_edit_config', $row=array(), $hidden=''){
-	set_request('horaire',_request('horaire')=='non'?'non':'oui');
-	set_request('inscription',_request('inscription')?1:0);
+function formulaires_editer_evenement_traiter(
+	$id_evenement = 'new',
+	$id_article = 0,
+	$retour = '',
+	$lier_trad = 0,
+	$config_fonc = 'evenements_edit_config',
+	$row = array(),
+	$hidden = ''
+) {
+	set_request('horaire', _request('horaire')=='non' ? 'non' : 'oui');
+	set_request('inscription', _request('inscription') ? 1 : 0);
 	include_spip('inc/date_gestion');
 	$erreurs = array();
-	$date_debut = verifier_corriger_date_saisie('debut',_request('horaire')=='oui',$erreurs);
-	$date_fin = verifier_corriger_date_saisie('fin',_request('horaire')=='oui',$erreurs);
-	set_request('date_debut',date('Y-m-d H:i:s',$date_debut));
-	set_request('date_fin',date('Y-m-d H:i:s',$date_fin));
+	$date_debut = verifier_corriger_date_saisie('debut', _request('horaire')=='oui', $erreurs);
+	$date_fin = verifier_corriger_date_saisie('fin', _request('horaire')=='oui', $erreurs);
+	set_request('date_debut', date('Y-m-d H:i:s', $date_debut));
+	set_request('date_fin', date('Y-m-d H:i:s', $date_fin));
 
-	$res = formulaires_editer_objet_traiter('evenement',$id_evenement,$id_article,0,$retour,$config_fonc,$row,$hidden);
+	$res = formulaires_editer_objet_traiter('evenement', $id_evenement, $id_article, 0, $retour, $config_fonc, $row, $hidden);
 	// si c'est une creation dans un article publie, passer l'evenement en publie
 	// l'article peut être renseigné/modifié par l'utilisateur dans le formulaire. On le retrouve.
 	if (!intval($id_evenement)
-		AND $id_article = sql_getfetsel('id_article', 'spip_evenements', 'id_evenement='.$res['id_evenement'])
-		AND objet_test_si_publie('article',$id_article)){
+	    and $id_article = sql_getfetsel('id_article', 'spip_evenements', 'id_evenement='.$res['id_evenement'])
+	    and objet_test_si_publie('article', $id_article)) {
 		// sera refuse si auteur pas autorise
-		evenement_modifier($res['id_evenement'],array('statut'=>'publie'));
+		evenement_modifier($res['id_evenement'], array('statut'=>'publie'));
 	}
 	// a la creation, documenter la date de creation
-	if (!intval($id_evenement))
-		evenement_modifier($res['id_evenement'],array('date_creation'=>date('Y-m-d H:i:s')));
+	if (!intval($id_evenement)) {
+		evenement_modifier($res['id_evenement'], array('date_creation'=>date('Y-m-d H:i:s')));
+	}
 
 	$id_evenement = $res['id_evenement'];
 	if ($res['redirect']) {
-		if (strpos($res['redirect'],'article')!==false){
-			$id_article = sql_getfetsel('id_article','spip_evenements','id_evenement='.intval($id_evenement));
-			$res['redirect'] = parametre_url($res['redirect'],'id_article',$id_article);
+		if (strpos($res['redirect'], 'article')!==false) {
+			$id_article = sql_getfetsel('id_article', 'spip_evenements', 'id_evenement='.intval($id_evenement));
+			$res['redirect'] = parametre_url($res['redirect'], 'id_article', $id_article);
 		}
 	}
 	return $res;
