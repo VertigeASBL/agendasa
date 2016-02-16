d88ad17488a0c793705aff0b5c357b49a4f79a8c
Debondt Didier <p@henix.be>
2016-02-16 10:39:13 +0100

    Règle de codage

--- agendasa_fonctions.php
+++ agendasa_fonctions.php
@@ -10,7 +10,7 @@
  */
 
 if (!defined('_ECRIRE_INC_VERSION')) {
-	return;
+    return;
 }
 
 function formulaires_editer_evenement_verifier(
@@ -22,32 +22,37 @@ function formulaires_editer_evenement_verifier(
 	$row = array(),
 	$hidden = ''
 ) {
-	$erreurs = formulaires_editer_objet_verifier('evenement',$id_evenement,array('titre','date_debut','date_fin'));
-	include_spip('inc/date_gestion');
+    $erreurs = formulaires_editer_objet_verifier('evenement', $id_evenement, array('titre', 'date_debut', 'date_fin'));
+    include_spip('inc/date_gestion');
 
-	$horaire = _request('horaire')=='non'?false:true;
-	if(!$erreurs['date_debut'])
-		$date_debut = verifier_corriger_date_saisie('debut',$horaire,$erreurs);
-	if(!$erreurs['date_fin'])
-		$date_fin = verifier_corriger_date_saisie('fin',$horaire,$erreurs);
+    $horaire = _request('horaire')=='non' ? false : true;
+    if (!$erreurs['date_debut']) {
+        $date_debut = verifier_corriger_date_saisie('debut', $horaire, $erreurs);
+    }
 
-	if ($date_debut AND $date_fin AND $date_fin<$date_debut)
-		$erreurs['date_fin'] = _T('agenda:erreur_date_avant_apres');
+    if (!$erreurs['date_fin']) {
+        $date_fin = verifier_corriger_date_saisie('fin', $horaire, $erreurs);
+    }
 
-	include_spip('formulaires/selecteur/selecteur_fonctions');
-	if (count($id = picker_selected(_request('parents_id'),'article'))
-		AND $id = reset($id)
-		AND $id = sql_getfetsel('id_article','spip_articles','id_article='.intval($id))){
-		// reinjecter dans id_parent
-		set_request('id_parent',$id);
-	}
+    if ($date_debut and $date_fin and $date_fin<$date_debut) {
+        $erreurs['date_fin'] = _T('agenda:erreur_date_avant_apres');
+    }
 
-	if (!$id_parent = intval(_request('id_parent')))
-		$erreurs['id_parent'] = _T('agenda:erreur_article_manquant');
-	else {
-		if (!autoriser('creerevenementdans','article',$id_parent))
-			$erreurs['id_parent'] = _T('agenda:erreur_article_interdit');
-	}
+    include_spip('formulaires/selecteur/selecteur_fonctions');
+    if (count($id = picker_selected(_request('parents_id'), 'article'))
+		and $id = reset($id)
+		and $id = sql_getfetsel('id_article', 'spip_articles', 'id_article='.intval($id))) {
+        // reinjecter dans id_parent
+		set_request('id_parent', $id);
+    }
+
+    if (!$id_parent = intval(_request('id_parent'))) {
+        $erreurs['id_parent'] = _T('agenda:erreur_article_manquant');
+    } else {
+        if (!autoriser('creerevenementdans', 'article', $id_parent)) {
+            $erreurs['id_parent'] = _T('agenda:erreur_article_interdit');
+        }
+    }
 
 	#if (!count($erreurs))
 	#	$erreurs['message_erreur'] = 'ok?';
