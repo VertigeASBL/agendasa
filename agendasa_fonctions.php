54b1b1a7f5c0f103c4062fc70d78ba7809dcefb8
Debondt Didier <p@henix.be>
2016-02-16 10:17:41 +0100

    Surcharge de la fonction de vérification d'événement

--- agendasa_fonctions.php
+++ agendasa_fonctions.php
@@ -12,3 +12,44 @@
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
+
+function formulaires_editer_evenement_verifier(
+	$id_evenement = 'new',
+	$id_article = 0,
+	$retour = '',
+	$lier_trad = 0,
+	$config_fonc = 'evenements_edit_config',
+	$row = array(),
+	$hidden = ''
+) {
+	$erreurs = formulaires_editer_objet_verifier('evenement',$id_evenement,array('titre','date_debut','date_fin'));
+	include_spip('inc/date_gestion');
+
+	$horaire = _request('horaire')=='non'?false:true;
+	if(!$erreurs['date_debut'])
+		$date_debut = verifier_corriger_date_saisie('debut',$horaire,$erreurs);
+	if(!$erreurs['date_fin'])
+		$date_fin = verifier_corriger_date_saisie('fin',$horaire,$erreurs);
+
+	if ($date_debut AND $date_fin AND $date_fin<$date_debut)
+		$erreurs['date_fin'] = _T('agenda:erreur_date_avant_apres');
+
+	include_spip('formulaires/selecteur/selecteur_fonctions');
+	if (count($id = picker_selected(_request('parents_id'),'article'))
+		AND $id = reset($id)
+		AND $id = sql_getfetsel('id_article','spip_articles','id_article='.intval($id))){
+		// reinjecter dans id_parent
+		set_request('id_parent',$id);
+	}
+
+	if (!$id_parent = intval(_request('id_parent')))
+		$erreurs['id_parent'] = _T('agenda:erreur_article_manquant');
+	else {
+		if (!autoriser('creerevenementdans','article',$id_parent))
+			$erreurs['id_parent'] = _T('agenda:erreur_article_interdit');
+	}
+
+	#if (!count($erreurs))
+	#	$erreurs['message_erreur'] = 'ok?';
+	return $erreurs;
+}
