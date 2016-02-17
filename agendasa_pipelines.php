ba0ae8531b3f7b14cda036e28ba31a7f3581e1bf
Debondt Didier <p@henix.be>
2016-02-17 15:00:17 +0100

    Cacher l'interface d'agenda sous le tapis

--- agendasa_pipelines.php
+++ agendasa_pipelines.php
@@ -13,6 +13,6 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
-function agendasa_affiche_milieu($flux) {
-	return $flux;
+function agendasa_header_prive($flux) {
+	return $flux.'<link rel="stylesheet" type="text/css" media="all" href="'.find_in_path('css/cacher_agenda.css').'" />';
 }
