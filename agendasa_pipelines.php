772b41d585bf1de3e18ca7bb18d999ebb1c48534
Debondt Didier <p@henix.be>
2016-03-11 11:22:25 +0100

    Restaurer la prévisualisation des événements

--- agendasa_pipelines.php
+++ agendasa_pipelines.php
@@ -16,3 +16,17 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 function agendasa_header_prive($flux) {
 	return $flux.'<link rel="stylesheet" type="text/css" media="all" href="'.find_in_path('css/cacher_agenda.css').'" />';
 }
+
+
+/**
+ * Restaurer la prévisualisation des événements
+ *
+ * @param array $tables
+ * @access public
+ * @return array
+ */
+function agendasa_declarer_tables_objets_sql($tables) {
+
+	$tables['spip_evenements']['statut'][0]['previsu'] = 'prop';
+	return $tables;
+}
